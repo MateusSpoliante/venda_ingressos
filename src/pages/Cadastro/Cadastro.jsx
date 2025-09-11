dc1e0a2d81311273fb3f028eef2efb0178310397
MateusSpoliante <mateus.spoliant11@gmail.com>
2025-09-10 21:49:49 -0300

    Localhost again

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -16,7 +16,7 @@ function Cadastro() {
     setSucesso("");
 
     try {
-      const resposta = await fetch("https://openticket.onrender.com/api/cadastro", {
+      const resposta = await fetch("http://localhost:3000/api/cadastro", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, senha }),
